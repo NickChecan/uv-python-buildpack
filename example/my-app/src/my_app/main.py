3fc41d41ff0a1c3451e985e8eff428e20b9aa850
NickChecan <nicholas.checan@gmail.com>
2026-04-09 14:01:48 -0500

    fix(common): cf apps should listen to port from the env

--- example/my-app/src/my_app/main.py
+++ example/my-app/src/my_app/main.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI
 import uvicorn
 
@@ -9,4 +11,5 @@ def hello_world():
     return {"message": "Hello, World!"}
 
 def start() -> None:
-    uvicorn.run("my_app.main:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", "8000"))
+    uvicorn.run("my_app.main:app", host="0.0.0.0", port=port, reload=False)
