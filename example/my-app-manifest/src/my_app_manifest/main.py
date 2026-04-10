2707ef064e4274f0669b4220b9301eaf2cd999ed
NickChecan <nicholas.checan@gmail.com>
2026-04-10 16:58:52 -0500

    chore(common): add manifest command example

--- example/my-app-manifest/src/my_app_manifest/main.py
+++ example/my-app-manifest/src/my_app_manifest/main.py
@@ -10,6 +10,6 @@ app = FastAPI()
 def hello_world():
     return {"message": "Hello, World!"}
 
-def start() -> None:
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", "8000"))
     uvicorn.run("my_app_manifest.main:app", host="0.0.0.0", port=port, reload=False)
