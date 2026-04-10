8b92e9f5990a5420628fd755e4ab861435ccad5a
NickChecan <nicholas.checan@gmail.com>
2026-04-10 08:19:42 -0500

    chore(common): add more examples

---
+++ example/my-app-script/src/my_app_script/main.py
@@ -0,0 +1,15 @@
+import os
+
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+def hello_world():
+    return {"message": "Hello, World!"}
+
+def start() -> None:
+    port = int(os.environ.get("PORT", "8000"))
+    uvicorn.run("my_app_script.main:app", host="0.0.0.0", port=port, reload=False)
